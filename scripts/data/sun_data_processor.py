450f7bb65f13c5615420516c6ea9e3fa5cd311e6
brum <funnybrum@gmail.com>
2022-08-26 08:56:27 +0300

    Changing the approach for min energy calculation

--- scripts/data/sun_data_processor.py
+++ scripts/data/sun_data_processor.py
@@ -1,9 +1,12 @@
 IN_FILE_NAME = "Timeseries_42.605_23.482_SA2_3kWp_crystSi_14_17deg_7deg_2020_2020.csv"
 FIRST_LINE = 10  # zero-based line index that contains the headers
-LOOK_AROUND_DAYS = 7
+LOOK_AROUND_DAYS = 7  # 14 for daily drops calculator
 OUTPUT_FILE = "avg_power.csv"
 
 import csv
+
+from scipy.interpolate import CubicSpline, interp1d
+
 
 output_data = []
 
@@ -64,3 +67,42 @@ with open(OUTPUT_FILE, 'w', newline='') as csv_file:
 
     for entry in output_data:
         writer.writerow([entry.get_date(), entry.get_min(), entry.get_max()])
+
+exit(1)
+
+drops = []
+
+for month in range(1, 13):
+    diffs = []
+    # print("=========== month %s ===========" % month)
+    for entry in output_data:
+        if entry.get_date().startswith("2020%02d" % month):
+            if entry.get_min() > 100 and entry.get_max() > 100:
+                diffs.append(100 * entry.get_min() / entry.get_max())
+
+    diffs = sorted(diffs)
+
+    # for p in [1, 2, 5, 10, 20, 50, 80, 90, 95, 98, 99]:
+    #     index = int(len(diffs) * p / 100)
+    #     print("P%s = %s" % (p, diffs[index]))
+
+    drops.append(int(diffs[int(len(diffs) * 98 / 100)]))
+
+days = [i for i in range(15, 365, 30)]
+days.insert(0, 0)
+days.append(365)
+
+drops.insert(0, drops[0])
+drops.append(drops[len(drops)-1])
+
+# print(days)
+print(drops)
+
+# Patched a bit to be smoother
+drops = [13, 13, 13, 13, 13, 18, 22, 31, 50, 55, 12, 12, 12, 12]
+
+cs = interp1d(days, drops)
+# min_sun_power = max(0, int(cs(target_y)))
+daily_drops = [int(cs(i)) for i in range(1, 366)]
+
+print(daily_drops)
--- scripts/sun_calc.py
+++ scripts/sun_calc.py
@@ -17,6 +17,7 @@ INTERPOLATION_POINTS_AROUND = 3
 DATES = []
 MIN_POWER = []
 MAX_POWER = []
+DAILY_DROPS = [13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 23, 23, 23, 24, 24, 24, 25, 25, 25, 25, 26, 26, 26, 27, 27, 27, 28, 28, 28, 28, 29, 29, 29, 30, 30, 30, 31, 31, 32, 32, 33, 34, 34, 35, 36, 36, 37, 37, 38, 39, 39, 40, 41, 41, 42, 43, 43, 44, 44, 45, 46, 46, 47, 48, 48, 49, 50, 50, 50, 50, 50, 50, 51, 51, 51, 51, 51, 51, 52, 52, 52, 52, 52, 52, 53, 53, 53, 53, 53, 53, 54, 54, 54, 54, 54, 54, 55, 53, 52, 50, 49, 47, 46, 44, 43, 42, 40, 39, 37, 36, 34, 33, 32, 30, 29, 27, 26, 24, 23, 22, 20, 19, 17, 16, 14, 13, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
 
 with open(IN_FILE_NAME, newline='') as csv_file:
     reader = csv.DictReader(csv_file)
@@ -39,9 +40,12 @@ def calculate(month, day, hour, minute):
     cs = CubicSpline(times, powers)
     max_sun_power = max(0, int(cs(target_y)))
 
-    powers = [MIN_POWER[i] for i in range(dates_index - INTERPOLATION_POINTS_AROUND, dates_index + INTERPOLATION_POINTS_AROUND + 1)]
-    cs = CubicSpline(times, powers)
-    min_sun_power = max(0, int(cs(target_y)))
+    # powers = [MIN_POWER[i] for i in range(dates_index - INTERPOLATION_POINTS_AROUND, dates_index + INTERPOLATION_POINTS_AROUND + 1)]
+    # cs = CubicSpline(times, powers)
+    # min_sun_power = max(0, int(cs(target_y)))
+
+    day_of_year = datetime.datetime.utcnow().timetuple().tm_yday
+    min_sun_power = DAILY_DROPS[day_of_year-1] * 0.01 * max_sun_power
 
     send_data("power",
               {
